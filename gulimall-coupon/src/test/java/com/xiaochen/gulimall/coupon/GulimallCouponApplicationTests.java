c77b1c18d079707a606415d13028bc95b24e0615
chenxiao <591343671@qq.com>
2021-07-18 13:46:32 +0800

    创建项目初始结构

---
+++ gulimall-coupon/src/test/java/com/xiaochen/gulimall/coupon/GulimallCouponApplicationTests.java
@@ -0,0 +1,13 @@
+package com.xiaochen.gulimall.coupon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GulimallCouponApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
---
+++ gulimall-member/src/test/java/com/xiaochen/gulimall/member/GulimallMemberApplicationTests.java
@@ -0,0 +1,13 @@
+package com.xiaochen.gulimall.member;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GulimallMemberApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
